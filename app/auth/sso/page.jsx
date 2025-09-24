4da058536301cc17e90aa5968adb5716a8e29730
dmostafiz <dev.mostafiz@gmail.com>
2025-09-24 16:50:52 +0600

    okay

--- app/auth/sso/page.jsx
+++ app/auth/sso/page.jsx
@@ -1,56 +1,11 @@
-'use client'
-
-import Cookies from 'js-cookie'
-import Axios from '@/Helpers/Axios'
-
-
-import React, { useEffect, useState } from 'react'
-import { useSearchParams } from "next/navigation"
+import React, { Suspense } from 'react'
 
 export default function page() {
-
-    const searchParams = useSearchParams()
-    const [loading, setLoading] = useState(true)
-    const [error, setError] = useState(null)
-
-    useEffect(() => {
-        const token = searchParams.get("_verification_token") // example: ?id=123
-
-        if (token) {
-            createPaymentIntent(token)
-        }
-
-
-    }, [])
-
-    async function createPaymentIntent(token) {
-
-        const res = await Axios.post('/uxlm/create-order-intent', {
-            amount: 7
-        }, {
-            headers: {
-                Authorization: `Bearer ${token}`
-            }
-        })
-
-        if (res?.data?.ok) {
-
-            Cookies.set('intent', res?.data?.intent?.client_secret)
-            Cookies.set('token', res?.data?.token)
-
-            return window.location.href = '/'
-
-        } else {
-            setError(res?.data?.msg)
-        }
-
-        setLoading(false)
-    }
-
     return (
-        <div className='h-full w-full flex items-center justify-center'>
-            {loading && <span>Verifying...</span>}
-            {error && <span className='text-red-500'>{error}</span>}
+        <div>
+            <Suspense>
+                <Sso />
+            </Suspense>
         </div>
     )
 }
